2f9b890b416c4f837f3174ef9851c1bdbcb294fb
Gokul <gokulr5102@gmail.com>
2022-01-11 19:35:46 +0530

    First commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
-module.exports = {
-  env: {
-    mongodb_username: "gokul",
-    mongodb_password: "tWJaDIcAXSxjYvIg",
-    mongodb_clustername: "cluster0",
-    mongodb_database: "my-site",
-  },
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "gokul",
+        mongodb_password: "tWJaDIcAXSxjYvIg",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "my-site",
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: "gokul",
+      mongodb_password: "tWJaDIcAXSxjYvIg",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "my-site",
+    },
+  };
 };
